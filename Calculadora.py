ddc90934f66cdcdd9a9ca9504a12900aa40b5391
irving-coder <irving.mtqz10@yahoo.com>
2021-01-09 21:41:47 -0600

    Agregando suma y resta

--- Calculadora.py
+++ Calculadora.py
@@ -1,1 +1,11 @@
-print('hola mundo')
+entrada1 = 8
+entrada2 = 5
+
+def suma(ent1, ent2):
+    return ent1 + ent2
+
+def resta(ent1, ent2):
+    return ent1 - ent2
+
+print('La suma de '+ str(entrada1) +' + '+ str(entrada2) +' es: '+ str(suma(entrada1, entrada2)))
+print('La resta de '+ str(entrada1) +' - '+ str(entrada2) +' es: '+ str(resta(entrada1, entrada2)))
